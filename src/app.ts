d54dabb25fe334f6c998abe114bc3b30853db27e
Esteban <etejedaw@hotmail.com>
2025-11-24 13:13:15 -0300

    chore: adjust imports and basic configuration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD!");
+import { environmentConfig } from "./common/config/environment.config";
+import { databaseInit } from "./database/database.init";
+import { server } from "./server";
+
+async function bootstrap() {
+	await databaseInit();
+	server(environmentConfig.PORT);
+}
+
+void bootstrap();
