513b8993d9f201cf330f7e707ab8bb04eb1af64e
Esteban <etejedaw@hotmail.com>
2025-11-24 17:58:58 -0300

    chore(common): add cors environment variables

--- src/common/config/environment.config.ts
+++ src/common/config/environment.config.ts
@@ -11,7 +11,11 @@ const EnvironmentConfigSchema = z
 		PG_PORT: z.coerce.number().default(5432),
 		ACCESS_TOKEN_TTL: z.union([z.string(), z.number()]).default("1d"),
 		ACCESS_TOKEN_SECRET: z.string().default("secret-token"),
-		PASSWORD_SALT_ROUNDS: z.number().default(10)
+		PASSWORD_SALT_ROUNDS: z.number().default(10),
+		CORS_ORIGIN: z.string().default("*"),
+		CORS_METHODS: z.string().default("*"),
+		CORS_ALLOWED_HEADERS: z.string().default("*"),
+		CORS_CREDENTIALS: z.boolean().default(false)
 	})
 	.readonly();
 
