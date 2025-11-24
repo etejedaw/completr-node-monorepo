ec42d68e9ea05508517d009f2b7b20c46fcfd087
Esteban <etejedaw@hotmail.com>
2025-11-24 17:44:06 -0300

    chore: add token and password environment variables

--- src/common/config/environment.config.ts
+++ src/common/config/environment.config.ts
@@ -2,13 +2,16 @@ import z from "zod";
 
 const EnvironmentConfigSchema = z
 	.object({
-		PORT: z.coerce.number().default(3000),
 		NODE_ENV: z.enum(["test", "dev", "stg", "prd"]).default("dev"),
+		PORT: z.coerce.number().default(3000),
 		PG_DATABASE: z.string().default("postgres"),
 		PG_USER: z.string().default("root"),
 		PG_PASSWORD: z.string().default("toor"),
 		PG_HOST: z.string().default("127.0.0.1"),
-		PG_PORT: z.coerce.number().default(5432)
+		PG_PORT: z.coerce.number().default(5432),
+		ACCESS_TOKEN_TTL: z.union([z.string(), z.number()]).default("1d"),
+		ACCESS_TOKEN_SECRET: z.string().default("secret-token"),
+		PASSWORD_SALT_ROUNDS: z.number().default(10)
 	})
 	.readonly();
 
