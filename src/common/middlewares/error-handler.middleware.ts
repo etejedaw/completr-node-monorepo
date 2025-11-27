a689a5702d20d42a6cf58510787e540915783d7f
Esteban <etejedaw@hotmail.com>
2025-11-27 04:02:23 -0300

    feat(common): add centralized error handler middleware

---
+++ src/common/middlewares/error-handler.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomRequest } from "../interfaces/custom-request.interface";
+import { globalErrorDomainNormalizer } from "../errors/global-error-domain.normalizer";
+import { globalErrorHttpNormalizer } from "../errors/global-error-http.normalizer";
+
+export function errorHandlerMiddleware(
+	error: Error,
+	request: Request,
+	response: Response,
+	_next: NextFunction
+) {
+	const customRequest = request as CustomRequest;
+	const { correlationId } = customRequest;
+
+	const domainError = globalErrorDomainNormalizer(error, correlationId);
+	const httpError = globalErrorHttpNormalizer(domainError, customRequest);
+
+	return response.status(httpError.status).json({
+		correlationId: httpError.correlationId,
+		type: httpError.type,
+		title: httpError.title,
+		status: httpError.status,
+		detail: httpError.detail,
+		instance: httpError.instance,
+		timestamp: httpError.timestamp,
+		context: httpError.context
+	});
+}
