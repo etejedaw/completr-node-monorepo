cfe0dd3a88db69dffb8540d36f7037722af02e12
Esteban <etejedaw@hotmail.com>
2025-12-29 20:26:09 -0300

    chore: update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,20 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig(
+	{ ignores: ["dist", "coverage", "node_modules"] },
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 	tseslint.configs.strict,
 	tseslint.configs.stylistic,
+	{
+		rules: {
+			"@typescript-eslint/no-non-null-assertion": "off",
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{ varsIgnorePattern: "^_", argsIgnorePattern: "^_" }
+			]
+		}
+	},
 	eslintConfigPrettier
 );
