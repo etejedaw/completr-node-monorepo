6052350e2e7e7119ef0ef39a92ec09c89aa14766
Esteban <etejedaw@hotmail.com>
2025-12-29 20:27:19 -0300

    style(common): apply style rules

--- src/common/middlewares/rate-limiter.middleware.ts
+++ src/common/middlewares/rate-limiter.middleware.ts
@@ -3,7 +3,11 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 import * as authDomainsErrors from "../../auth/errors/auth.domains-error";
 
 export function rateLimiterMiddleware(limiter: RateLimiterMemory) {
-	return async (request: Request, _response: Response, next: NextFunction) => {
+	return async (
+		request: Request,
+		_response: Response,
+		next: NextFunction
+	) => {
 		try {
 			const ip = request.ip;
 			await limiter.consume(ip!);
