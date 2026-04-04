d025ec48daad18e9a88f23113a15a8048767d3a0
Esteban <etejedaw@hotmail.com>
2026-04-04 03:02:56 -0300

    feat(game-times): add rawg as time source

--- src/game-times/game-time.model.ts
+++ src/game-times/game-time.model.ts
@@ -1,7 +1,7 @@
 import { DataTypes, Model, Sequelize } from "sequelize";
 import { sequelize } from "../database/sequelize.database";
 
-export type TimeSource = "hltb" | "backlogr";
+export type TimeSource = "hltb" | "rawg" | "backlogr";
 
 class GameTime extends Model {
 	declare id: string;
@@ -24,7 +24,7 @@ GameTime.init(
 			allowNull: false
 		},
 		source: {
-			type: DataTypes.ENUM("hltb", "backlogr"),
+			type: DataTypes.ENUM("hltb", "rawg", "backlogr"),
 			allowNull: false
 		},
 		duration: {
