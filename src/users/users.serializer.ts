ae7b0ef0590cadfa6058560a8d64562629d8a565
Esteban <etejedaw@hotmail.com>
2026-04-04 20:35:18 -0300

    refactor(users): split serializer into userMeSerializer and userPublicSerializer

--- src/users/users.serializer.ts
+++ src/users/users.serializer.ts
@@ -1,11 +1,22 @@
 import { User } from "./user.model";
 
-export function userSerializer(user: User) {
+export function userMeSerializer(user: User) {
 	return {
 		id: user.id,
 		username: user.username,
 		email: user.email,
 		role: user.role,
+		name: user.name,
+		bio: user.bio,
+		avatarUrl: user.avatarUrl,
+		isPublic: user.isPublic
+	};
+}
+
+export function userPublicSerializer(user: User) {
+	return {
+		id: user.id,
+		username: user.username,
 		name: user.name,
 		bio: user.bio,
 		avatarUrl: user.avatarUrl
