accd9c9ed680ca1a28435c0dac0bd9fb7098636a
Esteban <etejedaw@hotmail.com>
2026-04-04 19:02:40 -0300

    feat(game-shelf): implement getUserGameShelf controller with privacy checks

--- src/game-shelf/game-shelf.controller.ts
+++ src/game-shelf/game-shelf.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { RegisterGameShelfDto } from "./dtos/register-game-shelf.dto";
 import * as gameShelfService from "./game-shelf.service";
+import * as usersService from "../users/users.service";
+import * as userDomainError from "../users/errors/users.domain-error";
 import { CustomRequest } from "../common/interfaces/custom-request.interface";
 import { UpdateGameShelfDto } from "./dtos/update-game-shelf.dto";
 import { GameShelfIdParam } from "./schemas/game-shelf-id-params.schema";
-import { gameShelfMeSerializer, gameShelfSerializerTiny } from "./serializers";
+import { UsernameParam } from "../users/schemas/username-params.schema";
+import {
+	gameShelfMeSerializer,
+	gameShelfSerializer,
+	gameShelfSerializerTiny
+} from "./serializers";
 
 export async function postGameShelf(request: Request, response: Response) {
 	const customRequest = request as CustomRequest;
@@ -34,8 +41,21 @@ export async function getMeGameShelf(request: Request, response: Response) {
 	return response.status(200).json({ data });
 }
 
-export async function getUserGameShelf(_request: Request, _response: Response) {
-	// TODO
+export async function getUserGameShelf(request: Request, response: Response) {
+	const params = request.params as UsernameParam;
+	const { username } = params;
+
+	const user = await usersService.findUserByUsername(username);
+	if (!user) throw userDomainError.userNotFound();
+	if (!user.isPublic) throw userDomainError.userPrivate();
+
+	const gameShelf = await gameShelfService.findPublicGameShelfByUserId(
+		user.id
+	);
+	const gameShelfPlain = gameShelf.map(item => item.get({ plain: true }));
+
+	const data = { gameShelf: gameShelfPlain.map(gameShelfSerializer) };
+	return response.status(200).json({ data });
 }
 
 export async function patchGameShelf(request: Request, response: Response) {
