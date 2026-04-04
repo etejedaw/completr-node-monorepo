7a7d1fce05a0f1801147ac2faca4106ffe9d81cb
Esteban <etejedaw@hotmail.com>
2026-04-04 17:18:02 -0300

    refactor(game-shelf): remove score, duration and source fields from model

--- src/game-shelf/game-shelf.model.ts
+++ src/game-shelf/game-shelf.model.ts
@@ -13,10 +13,6 @@ class GameShelf extends Model {
 	declare acquiredAt?: Date;
 	declare edition?: string;
 	declare notes?: string;
-	declare score?: number;
-	declare duration?: number;
-	declare scoreSource?: string;
-	declare durationSource?: string;
 	declare createdAt: Date;
 	declare updatedAt: Date;
 	declare User: User;
@@ -50,11 +46,7 @@ GameShelf.init(
 		},
 		acquiredAt: DataTypes.DATE,
 		edition: DataTypes.STRING(100),
-		notes: DataTypes.STRING(100),
-		score: DataTypes.FLOAT,
-		duration: DataTypes.FLOAT,
-		scoreSource: DataTypes.STRING(50),
-		durationSource: DataTypes.STRING(50)
+		notes: DataTypes.STRING(100)
 	},
 	{ sequelize }
 );
