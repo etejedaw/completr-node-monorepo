5228ca3f8d1a90bc1f96d810e1f3a4bb73d49349
Esteban <etejedaw@hotmail.com>
2026-04-04 02:24:20 -0300

    feat(rawg): add RAWG API response interfaces

---
+++ src/rawg/rawg.interface.ts
@@ -0,0 +1,36 @@
+export interface RawgGameSearchResult {
+	id: number;
+	slug: string;
+	name: string;
+	released: string | null;
+	background_image: string | null;
+	metacritic: number | null;
+	playtime: number;
+	genres: RawgGenre[];
+	platforms: RawgPlatformWrapper[];
+}
+
+export interface RawgGameDetail extends RawgGameSearchResult {
+	description_raw: string | null;
+	website: string | null;
+}
+
+export interface RawgGenre {
+	id: number;
+	name: string;
+	slug: string;
+}
+
+export interface RawgPlatformWrapper {
+	platform: {
+		id: number;
+		name: string;
+		slug: string;
+	};
+}
+
+export interface RawgSearchResponse {
+	count: number;
+	next: string | null;
+	results: RawgGameSearchResult[];
+}
