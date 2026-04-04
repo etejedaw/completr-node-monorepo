a4bf7cee3b28e1a0e3aec3af3d2e565b0e377203
Esteban <etejedaw@hotmail.com>
2026-04-04 18:25:11 -0300

    feat(games): add scores and times to game serializer

--- src/games/games.serializer.ts
+++ src/games/games.serializer.ts
@@ -1,8 +1,13 @@
+import { GameScore } from "../game-scores/game-score.model";
+import { GameTime } from "../game-times/game-time.model";
 import { Genre } from "../genres/genres.model";
 import { Platform } from "../platforms/platform.model";
 import { Game } from "./game.model";
 
 export function gameSerializer(game: Game) {
+	const scores = game.GameScores?.map(scoreSerializer) ?? [];
+	const times = game.GameTimes?.map(timeSerializer) ?? [];
+
 	return {
 		id: game.id,
 		title: game.title,
@@ -14,7 +19,9 @@ export function gameSerializer(game: Game) {
 		parentGameId: game.parentGameId,
 		updatedAt: game.updatedAt,
 		platforms: game.Platforms.map(platformSerializer),
-		genres: game.Genres.map(genreSerializer)
+		genres: game.Genres.map(genreSerializer),
+		scores,
+		times
 	};
 }
 
@@ -32,5 +39,19 @@ function genreSerializer(genre: Genre) {
 		id: genre.id,
 		name: genre.name,
 		code: genre.code
+	};
+}
+
+function scoreSerializer(gameScore: GameScore) {
+	return {
+		source: gameScore.source,
+		score: gameScore.score
+	};
+}
+
+function timeSerializer(gameTime: GameTime) {
+	return {
+		source: gameTime.source,
+		duration: gameTime.duration
 	};
 }
