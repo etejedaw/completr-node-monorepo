b5383d909bd4c4f5bf19dedf17f5479e2f35136b
Esteban <etejedaw@hotmail.com>
2026-04-04 22:13:50 -0300

    feat(errors): register playthroughs in global normalizers

--- src/common/errors/global-error-domain.normalizer.ts
+++ src/common/errors/global-error-domain.normalizer.ts
@@ -6,6 +6,7 @@ import { genresErrorDomainNormalizer } from "../../genres/errors/genres.error-do
 import { gameShelfErrorDomainNormalizer } from "../../game-shelf/errors/game-shelf.error-domain.normalizer";
 import { gameScoresErrorDomainNormalizer } from "../../game-scores/errors/game-scores.error-domain.normalizer";
 import { gameTimesErrorDomainNormalizer } from "../../game-times/errors/game-times.error-domain.normalizer";
+import { playthroughsErrorDomainNormalizer } from "../../playthroughs/errors/playthroughs.error-domain.normalizer";
 import { DomainError } from "./domain-error";
 import { ServiceError } from "./service-error";
 
@@ -51,6 +52,9 @@ function globalServiceErrorMapper(
 
 	if (error.serviceError.service === "GameTime Service")
 		return gameTimesErrorDomainNormalizer(error, correlationId);
+
+	if (error.serviceError.service === "Playthrough Service")
+		return playthroughsErrorDomainNormalizer(error, correlationId);
 
 	return new DomainError("COMMON", "INTERNAL_ERROR", "Unexpected error", {
 		raw: error,
