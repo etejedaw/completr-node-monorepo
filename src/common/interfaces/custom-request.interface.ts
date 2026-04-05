d714a6f549488b17d3b33b238372c56419156952
Esteban <etejedaw@hotmail.com>
2026-04-05 05:24:56 -0400

    refactor(common): remove CustomRequest interface

--- src/common/interfaces/custom-request.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-import { User } from "../../users";
-
-export interface CustomRequest extends Request {
-	user: CustomUser;
-	correlationId: string;
-}
-
-type CustomUser = Pick<User, "id" | "username" | "email" | "role" | "isActive">;
