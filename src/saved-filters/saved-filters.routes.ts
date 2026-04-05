d5cf066e0e39cdf6fbd2f4a8200a38394543d223
Esteban <etejedaw@hotmail.com>
2026-04-05 02:32:04 -0400

    feat(saved-filters): add routes nested under /users/me/saved-filters

---
+++ src/saved-filters/saved-filters.routes.ts
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import * as savedFiltersController from "./saved-filters.controller";
+import { rateLimiterMiddleware } from "../common/middlewares/rate-limiter.middleware";
+import { userLimiter } from "../common/config/rate-limiter.config";
+import { authMiddleware } from "../auth/auth.middleware";
+import { validateSchemaMiddleware } from "../common/middlewares/validate-schema.middleware";
+import { RegisterSavedFilterSchema } from "./schemas/register-saved-filter.schema";
+import { UpdateSavedFilterSchema } from "./schemas/update-saved-filter.schema";
+import { SavedFilterIdParamsSchema } from "./schemas/saved-filter-id-params.schema";
+
+const router = Router({ mergeParams: true });
+
+router.post(
+	"/",
+	[
+		rateLimiterMiddleware(userLimiter),
+		authMiddleware("user", "premium", "moderator"),
+		validateSchemaMiddleware(RegisterSavedFilterSchema, "body")
+	],
+	savedFiltersController.postSavedFilter
+);
+
+router.get(
+	"/",
+	[
+		rateLimiterMiddleware(userLimiter),
+		authMiddleware("user", "premium", "moderator")
+	],
+	savedFiltersController.getMeSavedFilters
+);
+
+router.patch(
+	"/:filterId",
+	[
+		rateLimiterMiddleware(userLimiter),
+		authMiddleware("user", "premium", "moderator"),
+		validateSchemaMiddleware(SavedFilterIdParamsSchema, "params"),
+		validateSchemaMiddleware(UpdateSavedFilterSchema, "body")
+	],
+	savedFiltersController.patchSavedFilter
+);
+
+router.delete(
+	"/:filterId",
+	[
+		rateLimiterMiddleware(userLimiter),
+		authMiddleware("user", "premium", "moderator"),
+		validateSchemaMiddleware(SavedFilterIdParamsSchema, "params")
+	],
+	savedFiltersController.deleteSavedFilter
+);
+
+export default router;
