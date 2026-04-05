bdc283924aa6ce035692adbfb7a4ab26dbaba43d
Esteban <etejedaw@hotmail.com>
2026-04-04 22:13:57 -0300

    style(playthroughs): format register playthrough DTO

--- src/playthroughs/dtos/register-playthrough.dto.ts
+++ src/playthroughs/dtos/register-playthrough.dto.ts
@@ -1,6 +1,4 @@
 import z from "zod";
 import { RegisterPlaythroughSchema } from "../schemas/register-playthrough.schema";
 
-export type RegisterPlaythroughDto = z.infer<
-	typeof RegisterPlaythroughSchema
->;
+export type RegisterPlaythroughDto = z.infer<typeof RegisterPlaythroughSchema>;
