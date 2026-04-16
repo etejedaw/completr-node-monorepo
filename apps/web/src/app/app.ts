09c6b1da73e10736356d6e5c2aee7638fdbaf51d
Esteban <etejedaw@hotmail.com>
2026-04-16 14:17:24 -0400

    feat(pwa): auto-activate and reload on new service worker version

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { SwUpdate, VersionReadyEvent } from "@angular/service-worker";
+import { filter } from "rxjs";
 
 @Component({
 	selector: "app-root",
 	imports: [RouterOutlet],
 	template: "<router-outlet />"
 })
-export class App {}
+export class App {
+	private readonly swUpdate = inject(SwUpdate);
+
+	constructor() {
+		if (this.swUpdate.isEnabled) {
+			this.swUpdate.versionUpdates
+				.pipe(
+					filter(
+						(event): event is VersionReadyEvent =>
+							event.type === "VERSION_READY"
+					)
+				)
+				.subscribe(() => {
+					this.swUpdate
+						.activateUpdate()
+						.then(() => document.location.reload());
+				});
+		}
+	}
+}
