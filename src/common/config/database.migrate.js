07281910aa1c0ce3c45d6b27dabbbf66257dc56f
Esteban <etejedaw@hotmail.com>
2026-04-16 01:36:51 -0400

    fix(db): rename migration config keys to match NODE_ENV values

--- src/common/config/database.migrate.js
+++ src/common/config/database.migrate.js
@@ -1,5 +1,5 @@
 module.exports = {
-	development: {
+	dev: {
 		host: process.env.PG_HOST || "127.0.0.1",
 		port: process.env.PG_PORT || 5432,
 		username: process.env.PG_USER || "root",
@@ -7,7 +7,15 @@ module.exports = {
 		database: process.env.PG_DATABASE || "postgres",
 		dialect: "postgres"
 	},
-	production: {
+	stg: {
+		host: process.env.PG_HOST,
+		port: process.env.PG_PORT,
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		dialect: "postgres"
+	},
+	prd: {
 		host: process.env.PG_HOST,
 		port: process.env.PG_PORT,
 		username: process.env.PG_USER,
