d4dfd64225f64524ac3d15c19fb5571e63134284
Esteban <etejedaw@hotmail.com>
2026-05-21 02:17:53 -0400

    feat(help): redesign as paginated tutorial carousel

--- apps/web/src/app/features/help/help-page.ts
+++ apps/web/src/app/features/help/help-page.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	signal
+} from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -8,4 +13,34 @@ import { RouterLink } from "@angular/router";
 	templateUrl: "./help-page.html",
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HelpPage {}
+export class HelpPage {
+	protected readonly steps = [
+		"backlog",
+		"ratio",
+		"scores",
+		"statuses",
+		"modules",
+		"privacy"
+	] as const;
+
+	protected readonly step = signal(0);
+	protected readonly total = this.steps.length;
+	protected readonly current = computed(() => this.steps[this.step()]);
+	protected readonly canPrev = computed(() => this.step() > 0);
+	protected readonly canNext = computed(() => this.step() < this.total - 1);
+	protected readonly progress = computed(
+		() => ((this.step() + 1) / this.total) * 100
+	);
+
+	prev() {
+		if (this.canPrev()) this.step.set(this.step() - 1);
+	}
+
+	next() {
+		if (this.canNext()) this.step.set(this.step() + 1);
+	}
+
+	goTo(index: number) {
+		this.step.set(index);
+	}
+}
