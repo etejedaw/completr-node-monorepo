55fc8659c6e57dc35bbfb4296a211462ea612d7a
Esteban <etejedaw@hotmail.com>
2026-05-23 04:22:32 -0400

    chore(tooling): add prettier eslint husky lint-staged

--- apps/landing/eslint.config.js
+++ apps/landing/eslint.config.js
@@ -1,9 +1,11 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
 
 export default [
+	...tseslint.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
 	{
-		ignores: [".astro/**"]
+		ignores: [".astro/**", "dist/"]
 	},
 	{
 		rules: {
